5acf3f6b3ad5b2be05b99a2cd37f22a283299d6c
John Jordan <john.jordan@sendsafely.com>
2023-02-02 14:59:52 -0500

    add: external validation for State (from ZIP) using USPS database

--- standard-list/de-dupe.js
+++ standard-list/de-dupe.js
@@ -1,64 +1,35 @@
 const fs = require("fs");
+const validate = require("./validate-state-zip");
 const raw = fs.readFileSync('ein-api-results.json', 'utf-8');
-const a = JSON.parse(raw).data;
+const companyNameSearchResults = JSON.parse(raw).data;
 
-// const a = [
-//         {
-//         "companyId": "31893961",
-//         "ein": "900351836",
-//         "companyName": "AMAZON COM",
-//         "address": "1453 E 10TH ST",
-//         "address2": "",
-//         "city": "JEFFERSONVILLE",
-//         "state": "IN",
-//         "zipCode": "471304204",
-//         "telephone": null,
-//         "fax": "",
-//         "website": null,
-//         "firstName": "",
-//         "middleName": "",
-//         "lastName": "",
-//         "email": "",
-//         "sic": null,
-//         "totalEmployees": null,
-//         "salesRevenue": null,
-//         "headQuarter": "",
-//         "ncoa": null,
-//         "dateAcquired": "2016 or older",
-//         "ncoaUpdateDate": "2022-04",
-//         "otherPossibleNames": null
-//     },
-//     {
-//         "companyId": "31893961",
-//         "ein": "900351836",
-//         "companyName": "AMAZON COM",
-//         "address": "1453 E 10TH ST",
-//         "address2": "",
-//         "city": "JEFFERSONVILLE",
-//         "state": "IN",
-//         "zipCode": "471304204",
-//         "telephone": null,
-//         "fax": "",
-//         "website": null,
-//         "firstName": "",
-//         "middleName": "",
-//         "lastName": "",
-//         "email": "",
-//         "sic": null,
-//         "totalEmployees": null,
-//         "salesRevenue": null,
-//         "headQuarter": "",
-//         "ncoa": null,
-//         "dateAcquired": "2016 or older",
-//         "ncoaUpdateDate": "2022-04",
-//         "otherPossibleNames": null
-//     }
-// ];
-
-let b = a.filter((rec, currIndex, self) => {
+let deDupedEINs = companyNameSearchResults.filter((rec, currIndex, self) => {
     const firstIndexPos = self.findIndex(t => t.ein === rec.ein);
     // not a duplicate if first occurrence is same as current index position
     return currIndex === firstIndexPos;
 });
 
-console.log(a.length, b.length);
+console.log(companyNameSearchResults.length, deDupedEINs.length);
+
+if(!deDupedEINs.length) {
+    // no results for that company name
+    // return response to client, indicating optimistic update (since Name Search is broken, and can validate TIN-Name match)
+    /*
+        can validate customer submission EIN against name manually since IRS database available realtime
+     */
+}
+if(deDupedEINs.length === 1) {
+    // one result, proceed to TIN validation
+    const testCase = deDupedEINs[0];
+    const fiveDigitZIP = testCase.zipCode.slice(0,5)
+    console.log(testCase.state, fiveDigitZIP, validate.getState(fiveDigitZIP) === testCase.state);
+
+    if(validate.getState(fiveDigitZIP) !== testCase.state) {
+        //TODO: email that ZIP code/State validation has failed
+    }
+}
+if(deDupedEINs.length > 1) {
+    // duplicates
+    // return response to client, indicating optimistic update (since Name Search is broken, and can validate TIN-Name match)
+    // will require manual parsing of names to find
+}
