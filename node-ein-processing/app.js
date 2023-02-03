77280acb29afe83fd033967120831afbf2f0f47b
John Jordan <john.jordan@sendsafely.com>
2023-02-03 16:09:32 -0500

    add: parsing of results based on de-duped responses

--- node-ein-processing/app.js
+++ node-ein-processing/app.js
@@ -35,19 +35,18 @@ const c = {
 
 (async function(){
     try {
-        const apiResponse = await companyLookup(c.companyName);
+        //const apiResponse = await companyLookup(c.companyName);
+        const apiResponse = await companyLookup('MBP Group');
         const responseStatus = apiResponse.status; //'Success' or 'Error'
         const companyNameSearchResults = apiResponse.data;
 
-        console.log('companyNameSearchResults: ', companyNameSearchResults)
+        if('success' !== responseStatus.toLowerCase()) {
+            console.log('Error in connecting to EIN service');
+            return;
+        }
 
-        let deDupedEINs = companyNameSearchResults.filter((rec, currIndex, self) => {
-            const firstIndexPos = self.findIndex(t => t.ein === rec.ein);
-            // not a duplicate if first occurrence is same as current index position
-            return currIndex === firstIndexPos;
-        });
 
-        if(!deDupedEINs.length) {
+        if(!companyNameSearchResults.length) {
             // no results for that company name
             // return response to client, indicating optimistic update (since Name Search is broken, and can validate TIN-Name match)
             /*
@@ -55,13 +54,23 @@ const c = {
              */
             console.log(`no results available for this company (${c.companyName})`);
         }
+
+        let deDupedEINs = companyNameSearchResults.filter((rec, currIndex, self) => {
+            const firstIndexPos = self.findIndex(t => t.ein === rec.ein);
+            // not a duplicate if first occurrence is same as current index position
+            return currIndex === firstIndexPos;
+        });
+
+        console.log('Number of matches returned: ', deDupedEINs.length);
+
         if(deDupedEINs.length === 1) {
+            console.log('One result found...', companyNameSearchResults[0])
             // one result, proceed to TIN validation
-            const testCase = deDupedEINs[0];
+            const testCase = companyNameSearchResults[0];
             const fiveDigitZIP = testCase.zipCode.slice(0,5)
             console.log(testCase.state, fiveDigitZIP, validateZIP.getState(fiveDigitZIP) === testCase.state);
 
-            if(validate.getState(fiveDigitZIP) !== testCase.state) {
+            if(validateZIP.getState(fiveDigitZIP) !== testCase.state) {
                 //TODO: email that ZIP code/State validation has failed
             } else {
                 console.log('Validation of Zip confirmed...')
@@ -72,6 +81,8 @@ const c = {
             // duplicates
             // return response to client, indicating optimistic update (since Name Search is broken, and can validate TIN-Name match)
             // will require manual parsing of names to find
+
+            console.log('deDupedEINs: ', deDupedEINs);
         }
     } catch(e) {
         console.error(e);
